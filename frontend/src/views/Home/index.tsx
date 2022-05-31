e07f5b920cded1e513c1842ee8a57d95fdea0422
Wagner Terry <goncalveswagner15@gmail.com>
2022-05-31 10:47:33 -0300

    feat: dark mode

--- frontend/src/views/Home/index.tsx
+++ frontend/src/views/Home/index.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+import "./style.scss"
 
 export function Home(){
+    const [darkMode, setDarkMode] = useState(false)
+
+    function screenDarkMode(){
+        let element = document.body;
+        element.classList.toggle("darkmode")
+    } 
+
     return (
-        <h1>Home</h1>
+        <div id="home">
+            <header>
+                <div></div>
+                <h1>Mercadinho Banco de Dados</h1>
+                <button onClick={screenDarkMode}>DarkMode</button>
+            </header>
+        </div>
     )
 }
