9508f268c660dec48416018d7255101750694db4
Wagner Terry <goncalveswagner15@gmail.com>
2022-05-30 21:26:04 -0300

    chore: montando a estrutura de routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import {Routess}  from './Routess'
 
 function App() {
 
   return (
     <div>
-     <h1>Hello World</h1>
+      <Routess />
     </div>
   )
 }
---
+++ frontend/src/Routess.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from './views/Home';
+import { Users } from './views/Users';
+
+export function Routess(){
+    return (
+        <Router>
+            <div id="route">
+                <Routes>
+                    <Route path={"/"} exact element={Home}  />
+                    <Route path={'/users'} element={Users} />
+                </Routes>
+            </div>
+        </Router>
+    )
+}
