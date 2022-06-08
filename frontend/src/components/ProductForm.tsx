f7dc4d6706fb7e3586f74f18e6c67eb4a6bd5f9e
Wagner Terry <goncalveswagner15@gmail.com>
2022-06-07 23:05:49 -0300

    chore: escrevendo dentro de um input com as info

--- frontend/src/components/ProductForm.tsx
+++ frontend/src/components/ProductForm.tsx
@@ -18,58 +18,58 @@ interface ProductFormProps {
 
 //const [products, setProducts] = useState([] as any)
 
-async function addProduct(data: any) {
-    if (!data.id) {
-        try {
-            console.log("add product")
-            await APIService.insertProduct(data)
-            toast.success("Produto criado com sucesso")
-            //reset()
-            console.log("produto cadastrado: ", data)
-        } catch (e) {
-            toast.error("Erro ao criar produto")
-            console.log("Ocorreu um erro ao criar produto", e)
-        }
-    }
-    else {
-        changeProduct(data.id, data)
-    }
-}
-
-async function changeProduct(id: string, data: string) {
-    try {
-        console.log("update entrou")
-        await APIService.updateProducts(data)
-        toast.success("Produto atualizado")
-        console.log("atualizado", data)
-        //reset()
-    } catch (e) {
-        toast.error("Erro ao atualizar produto")
-        console.log("Ocorreu um erro ao atualizar produto")
-    }
-}
-
 export default function ProductForm(props: ProductFormProps) {
     const { register, handleSubmit, reset } = useForm({
         resolver: yupResolver(schema)
     });
+
+    async function addProduct(data: any) {
+        if (!data.id) {
+            try {
+                console.log("add product")
+                await APIService.insertProduct(data)
+                toast.success("Produto criado com sucesso")
+                reset()
+                console.log("produto cadastrado: ", data)
+            } catch (e) {
+                toast.error("Erro ao criar produto")
+                console.log("Ocorreu um erro ao criar produto", e)
+            }
+        }
+        else {
+            changeProduct(data.id, data)
+        }
+    }
+
+    async function changeProduct(id: string, data: string) {
+        try {
+            console.log("update entrou")
+            await APIService.updateProducts(data)
+            toast.success("Produto atualizado")
+            console.log("atualizado", data)
+            reset()
+        } catch (e) {
+            toast.error("Erro ao atualizar produto")
+            console.log("Ocorreu um erro ao atualizar produto")
+        }
+    }
     return (
 
         <form onSubmit={handleSubmit(addProduct)}>
             <input type="text"
                 placeholder='Nome do produto'
-                value={props.name}
+                defaultValue={props.name}
                 {...register("name")}
             />
             <input type="text"
                 placeholder='Descrição'
-                value={props.description}
+                defaultValue={props.description}
                 {...register("description")}
 
             />
             <input type="text"
                 placeholder='Imagem'
-                value={props.image}
+                defaultValue={props.image}
                 {...register("image")}
             />
             <input type="submit" value={"Salvar"} />
